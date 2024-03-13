9d063cbec6456997b66c11958173840352b4cd8c
Pratap Kotala <pratap.kotala@ndsu.edu>
2024-03-13 11:57:31 -0500

    Added login screen

--- website/views.py
+++ website/views.py
@@ -1,6 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
 
 def home(request):
     return render(request, 'website/home.html', {})
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'You have been authenticated and logged in successfully')
+            return redirect('home')
+        else:
+            messages.success(request, 'Login failed, try again!')
+            return redirect('login')
+    else:
+        return render(request, 'website/login.html', {})          
+
+
+
