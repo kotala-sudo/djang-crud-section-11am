394aa751ea9062755eff23b45c747c3952a959ee
Pratap Kotala <pratap.kotala@ndsu.edu>
2024-03-20 11:52:30 -0500

    Implemented read and delete employees CRUD operations

--- website/urls.py
+++ website/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.home, name='home' ),
     path('login/', views.login_user, name='login'),
     path('logout/', views.logout_user, name='logout'),
+    path('employee/<int:pk>/', views.employee_view, name="employee_detail"),
+    path('delete/<int:pk>/', views.delete, name="delete")
 ]
--- website/views.py
+++ website/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .models import Employee
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'website/home.html', {})
+    employee_list = Employee.objects.all()
+    return render(request, 'website/home.html', {'emp_list' : employee_list})
 
 def login_user(request):
     if request.method == "POST":
@@ -27,4 +29,21 @@ def logout_user(request):
     logout(request)
     messages.success(request, 'You have been logged out successfully')
     return redirect('home')
+
+def employee_view(request, pk):
+    if request.user.is_authenticated:
+        current_employee = Employee.objects.get(pk=pk)
+        return render(request, 'website/employee.html', {'emp' : current_employee})
+    else:
+        messages.success(request, 'You need to be logged in to see employee details')
+        return redirect('home')
+    
+def delete(request, pk):
+    if request.user.is_authenticated:
+        current_employee = Employee.objects.get(pk=pk)
+        current_employee.delete()
+        return redirect('home')
+    else:
+        messages.success(request, 'You need to be logged in delete the employee')
+        return redirect('home')
 
